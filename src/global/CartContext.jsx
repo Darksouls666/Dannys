17fc9ab381590d1ad8269bf05ed4dea431d9037e
unknown <edgarraegar@gmail.com>
2023-11-29 20:36:23 -0600

    Sitio completo

--- src/global/CartContext.jsx
+++ src/global/CartContext.jsx
@@ -1,15 +1,22 @@
-import React, { createContext, useReducer } from 'react'
-import { CartReducer } from './CartReducer'
+import React, { createContext, useReducer, useEffect } from 'react';
+import { CartReducer } from './CartReducer';
 
 export const CartContext = createContext();
 
 export const CartContextProvider = (props) => {
+    // Utilizar localStorage para obtener el estado inicial del carrito si está almacenado
+    const storedCart = JSON.parse(localStorage.getItem('shoppingCart')) || { shoppingCart: [], totalPrice: 0, totalQty: 0 };
 
-    const [cart, dispatch] = useReducer(CartReducer, { shoppingCart: [], totalPrice: 0, totalQty: 0 })
+    const [cart, dispatch] = useReducer(CartReducer, storedCart);
+
+    // Guardar en localStorage cada vez que cambie el carrito
+    useEffect(() => {
+        localStorage.setItem('shoppingCart', JSON.stringify(cart));
+    }, [cart]);
 
     return (
         <CartContext.Provider value={{ ...cart, dispatch }}>
             {props.children}
         </CartContext.Provider>
-    )
-}
+    );
+};
